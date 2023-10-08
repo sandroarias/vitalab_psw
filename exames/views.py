a7df65d7b1132f12b633711e71c64bed3ad3c972
sandroarias <sandro901217@gmail.com>
2023-10-08 17:14:12 -0300

    feat: Finalização do projeto

--- exames/views.py
+++ exames/views.py
@@ -4,31 +4,32 @@ from datetime import date, datetime
 from django.contrib import messages
 from django.contrib.messages import constants
 
-from .models import TiposExames, PedidosExames, SolicitacaoExame
+from .models import TiposExames, PedidosExames, SolicitacaoExame, AcessoMedico
 
-#@login_required
+
+@login_required
 def solicitar_exames(request):
-
     data_atual = date.today()
     tipos_exames = TiposExames.objects.all()
     if request.method == "GET":
-        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames}) 
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
     elif request.method == "POST":
         exames_id = request.POST.getlist('exames')
         solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
 
-        #TODO: Calcular preco dos dados disponiveis
-        preco_total = 0 
+        # TODO: Calcular preco dos dados disponiveis
+        preco_total = 0
         for i in solicitacao_exames:
             if i.disponivel:
                 preco_total += i.preco
 
         return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames,
-                                                          'preco_total': preco_total, 
-                                                          'solicitacao_exames': solicitacao_exames,
-                                                          'data_atual': data_atual})
-    
- #@login_required   
+                                                         'preco_total': preco_total,
+                                                         'solicitacao_exames': solicitacao_exames,
+                                                         'data_atual': data_atual})
+
+
+@login_required
 def fechar_pedido(request):
     exames_id = request.POST.getlist('exames')
     soliciatacao_exames = TiposExames.objects.filter(id__in=exames_id)
@@ -52,13 +53,15 @@ def fechar_pedido(request):
     messages.add_message(request, constants.SUCCESS, 'Pedido de exames realizado com sucesso!')
     return redirect('/exames/gerenciar_pedidos')
 
-#@login_required
+
+@login_required
 def gerenciar_pedidos(request):
     pedidos_exames = PedidosExames.objects.filter(usuario=request.user)
 
     return render(request, 'gerenciar_pedidos.html', {'pedidos_exames': pedidos_exames})
 
-#@login_required
+
+@login_required
 def cancelar_pedido(request, pedido_id):
     pedido = PedidosExames.objects.get(id=pedido_id)
     if not pedido.usuario == request.user:
@@ -69,7 +72,71 @@ def cancelar_pedido(request, pedido_id):
     messages.add_message(request, constants.SUCCESS, 'Pedido cancelado com sucesso!')
     return redirect('/exames/gerenciar_pedidos/')
 
-#@login_required
+
+@login_required
 def gerenciar_exames(request):
     exames = SolicitacaoExame.objects.filter(usuario=request.user)
     return render(request, 'gerenciar_exames.html', {'exames': exames})
+
+
+@login_required
+def perimitir_abrir_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+    if not exame.requer_senha:
+        # TUDO: verificar se tem o PDF do resultado
+        return redirect(exame.resultado.url)
+    return redirect(f'/exames/solicitar_senha_exame/{exame_id}')
+
+
+@login_required
+def solicitar_senha_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+    if request.method == "GET":
+        return render(request, 'solicitar_senha_exame.html', {'exame': exame})
+    elif request.method == "POST":
+        senha = request.POST.get('senha')
+        if senha == exame.senha:
+            # TUDO: verificar se tem o PDF do resultado
+            return redirect(exame.resultado.url)
+        else:
+            messages.add_message(request, constants.ERROR, 'Senha inválida.')
+            return redirect(f'/exames/solicitar_senha_exame/{exame_id}')
+
+
+@login_required
+def gerar_acesso_medico(request):
+    if request.method == "GET":
+        acessos_medicos = AcessoMedico.objects.filter(usuario=request.user)
+        return render(request, 'gerar_acesso_medico.html', {'acessos_medicos': acessos_medicos})
+    elif request.method == "POST":
+        identificacao = request.POST.get('identificacao')
+        tempo_de_acesso = request.POST.get('tempo_de_acesso')
+        data_exame_inicial = request.POST.get("data_exame_inicial")
+        data_exame_final = request.POST.get("data_exame_final")
+
+        acesso_medico = AcessoMedico(
+            usuario=request.user,
+            identificacao=identificacao,
+            tempo_de_acesso=tempo_de_acesso,
+            data_exames_iniciais=data_exame_inicial,
+            data_exames_finais=data_exame_final,
+            criado_em=datetime.now()
+        )
+
+        acesso_medico.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Acesso gerado com sucesso')
+        return redirect('/exames/gerar_acesso_medico')
+
+
+def acesso_medico(request, token):
+    acesso_medico = AcessoMedico.objects.get(token=token)
+    if acesso_medico.status == "Expirado":
+        messages.add_message(request, constants.ERROR, "Esse token já expirou, solicite outro.")
+        return redirect('/usuarios/login')
+
+    pedidos = PedidosExames.objects.filter(usuario=acesso_medico.usuario).filter(data__gte=acesso_medico.data_exames_iniciais).filter(data__lte=acesso_medico.data_exames_iniciais)
+
+    return render(request, 'acesso_medico.html', {'pedidos': pedidos})
+
+
